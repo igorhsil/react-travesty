751023f5cc5dd9d5146f271141e8e5a96dd32972
igorhsil <igorh@swissinnolab.com>
2022-05-19 12:33:58 +0200

    delete/filter feedback items

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import feedbackData from './components/data/seed';
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete a feedback?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,14 +1,15 @@
+import { logDOM } from '@testing-library/react';
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 
-export default function FeedbackList({ feedback }) {
+export default function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <h1>No feedback data.. </h1>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((f) => (
-        <FeedbackItem key={f.id} item={f} />
+        <FeedbackItem key={f.id} item={f} handleDelete={handleDelete} />
       ))}
     </div>
   );
